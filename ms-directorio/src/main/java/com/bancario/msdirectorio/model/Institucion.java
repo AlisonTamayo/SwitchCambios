1246a574ccfe1e1e828c157d8ff90ff68220bf79
MerlynUlcuango <mpulcuango@espe.edu.ec>
2026-01-11 00:04:47 -0500

    Cambios fase 0 migración v1 Ulcuango

--- ms-directorio/src/main/java/com/bancario/msdirectorio/model/Institucion.java
+++ ms-directorio/src/main/java/com/bancario/msdirectorio/model/Institucion.java
@@ -1,73 +1,28 @@
 package com.bancario.msdirectorio.model;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import java.util.Objects;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.*;
+import java.util.List;
+import java.util.ArrayList;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@Entity
-@Table(name = "Institucion")
-@Getter
-@Setter
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"}) 
+@Document(collection = "directorio") 
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Institucion {
 
     @Id
-    @Column(name = "codigoBic", length = 11, nullable = false)
-    private String codigoBic;
-
-    @Column(name = "nombre", nullable = false)
+    private String codigoBic; 
     private String nombre;
-
-    @Column(name = "urlDestino", nullable = false)
     private String urlDestino;
-
-    @Column(name = "llavePublica", columnDefinition = "TEXT", nullable = false)
     private String llavePublica;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "estadoOperativo", nullable = false)
     private EstadoOperativo estadoOperativo;
 
-    // CONSTRUCTOR VACÍO (Requerimiento 7)
-    public Institucion() {
-    }
-
-    // CONSTRUCTOR SOLO CLAVE PRIMARIA (Requerimiento 8)
-    public Institucion(String codigoBic) {
-        this.codigoBic = codigoBic;
-    }
-
-    // EQUALS & HASHCODE SOLO PK (Requerimiento 11)
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Institucion that = (Institucion) o;
-        return Objects.equals(codigoBic, that.codigoBic);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(codigoBic);
-    }
-
-    // TOSTRING COMPLETO (Requerimiento 12)
-    @Override
-    public String toString() {
-        return "Institucion{" +
-                "codigoBic='" + codigoBic + '\'' +
-                ", nombre='" + nombre + '\'' +
-                ", urlDestino='" + urlDestino + '\'' +
-                ", llavePublica='" + llavePublica + '\'' +
-                ", estadoOperativo=" + estadoOperativo +
-                '}';
-    }
+    private InterruptorCircuito interruptorCircuito; 
+    private List<ReglaEnrutamiento> reglasEnrutamiento = new ArrayList<>();
 }
 
-// Enum auxiliar para el estado
 enum EstadoOperativo {
     ONLINE, OFFLINE
 }
